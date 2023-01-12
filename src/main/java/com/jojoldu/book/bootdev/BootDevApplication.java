fcc025c45785ab7810a2a0f3df520e7cfc5c2674
shoney <ahdzl147@gmail.com>
2023-01-12 22:32:14 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/book/bootdev/BootDevApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.bootdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootDevApplication.class, args);
+    }
+
+}
